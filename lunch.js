d6985a2fff3b9b7a837d5d375b2f46c5103ba228
Martin Polden <martin.polden@gmail.com>
2013-01-01 15:01:16 +0100

    Linted

--- lunch.js
+++ lunch.js
@@ -2,35 +2,35 @@
 
 /*jslint node: true, es5: true*/
 
-"use strict";
+'use strict';
 
 var fs = require('fs'),
-    moment = require('moment'),
-    path = require('path'),
-    request = require('request'),
-    $ = require('jquery');
+  moment = require('moment'),
+  path = require('path'),
+  request = require('request'),
+  $ = require('jquery');
 
 if (process.argv.length < 3) {
-    console.log('usage:', process.argv.join(' '), '<path>');
-    process.exit(1);
+  console.log('usage:', process.argv.join(' '), '<path>');
+  process.exit(1);
 }
 
 request('http://www.dagbladet.no/tegneserie/lunch/', function (e, r, body) {
-    var img, imageUrl, date, savePath, filePath;
+  var img, imageUrl, date, savePath, filePath;
 
-    savePath = process.argv[2];
+  savePath = process.argv[2];
 
-    if (!e && r.statusCode === 200) {
-        img = $(body).find('img#lunch-stripe');
-        imageUrl = img.attr('src');
-        date = moment().format('YYYY-MM-DD');
-        filePath = path.join(savePath, date + '.gif');
-        fs.exists(filePath, function (exists) {
-            if (!exists) {
-                request(imageUrl).pipe(fs.createWriteStream(filePath));
-            }
-        });
-    } else {
-        console.log('Failed to scrape:', e);
-    }
+  if (!e && r.statusCode === 200) {
+    img = $(body).find('img#lunch-stripe');
+    imageUrl = img.attr('src');
+    date = moment().format('YYYY-MM-DD');
+    filePath = path.join(savePath, date + '.gif');
+    fs.exists(filePath, function (exists) {
+      if (!exists) {
+        request(imageUrl).pipe(fs.createWriteStream(filePath));
+      }
+    });
+  } else {
+    console.log('Failed to scrape:', e);
+  }
 });
